d5974f2580789efda41f374be8011bbb22c67f9b
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-05-27 15:00:35 +0530

    Added Modal file to the project and used it as wrapper for the cart, also used added css to overlay the modal and backdrop to blur the background

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import Header from "./components/Layout/Header";
 import { Fragment } from "react";
 import React from "react";
 import Meals from "./components/Meals/Meals";
-
+import Cart from "./components/Cart/Cart";
 const App = () => {
   return (
     <Fragment>
+         <Cart/> 
       <Header />
-      <main style={{backgroundColor: "#383838"}}>
-     <Meals/>
+      <main style={{ backgroundColor: "#383838" }}>
+        <Meals />
       </main>
     </Fragment>
   );
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,26 +1,31 @@
 import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
 const Cart = props => {
     const cartItems = <ul className={classes["cart-items"]}>
         {[{
             id: "c1",
             name: "Sahi Paneer",
             amount: 2,
-            price: 12.99,
-        }].map((item) =>
-            <li>
+            price: 12.99
+        }].map((item) => {
+            return (
+                   <li key={item.id}>
                 {item.name}
-        </li>)}   
+        </li>
+            );
+        }
+        )}   
     </ul>
-    return <div>
+    return <Modal>
         {cartItems}
         <div className={ classes.total}>
             <span>Total Amount</span>
             <span>35.62</span>
         </div>
         <div className={classes.actions}>
-        <buttons className={classes["button--alt"]}>Close</buttons>
+        <button className={classes["button--alt"]}>Close</button>
         <button className={classes.button}>Order</button>
         </div>
-  </div>  
+  </Modal>  
 };
 export default Cart;

1e60d637aa154c70e16b39bb189456efdda4c310
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-05-27 22:08:11 +0530

    Added useReducer hook to manage the state while adding the items to the cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,14 +1,41 @@
 import CartContext from "./cart-context";
+import { useReducer } from "react";
+
+const defaultCartState = {
+    items: [],
+    totalAmount:0
+}
+
+const cartReducer = (state, action) => {
+    
+    if (action.type === "ADD") {
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount =
+            state.totalAmount + action.item.price * action.item.amount;  
+        return ({
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        })
+    }
+    
+    return defaultCartState;
+};
 
 const CartProvider = props => {
 
-    const addItemCartHandeler = () => { };
+  const[cartState,dispatchCartAction] = useReducer(cartReducer,defaultCartState);
+
+    const addItemCartHandeler = (item) => {
+        dispatchCartAction({type:"ADD",item:item});
+     };
  
-    const removeCartItemHandeler = (id) => { };
+    const removeCartItemHandeler = (id) => {
+        dispatchCartAction({type:"REMOVE",id:id})
+     };
     
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem:addItemCartHandeler ,
         removeItem: removeCartItemHandeler
     };
